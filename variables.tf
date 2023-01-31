dc930dbb547df3720e66535b839776943b9e8e05
Ricardo Montuan <ricardo.montuan@hotmail.com>
2023-01-31 12:18:04 -0300

    chore: create initial vars and configure aws provider

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS Region"
+}
+
+variable "access_key" {
+  description = "The access key from the user used to manage resources"
+}
+
+variable "secret_key" {
+  description = "The secret key from the user used to manage resources"
+}
